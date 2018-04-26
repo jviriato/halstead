8fe065e7dee4ee0b5d88610c178fca9cb28fe3de
José Victor Viriato <jvviriato@inf.ufsm.br>
2018-04-26 09:53:01 -0300

    docs + loc

--- halstead.py
+++ halstead.py
@@ -2,27 +2,49 @@ import math
 import argparse # para argumentos em linha de comando
 
 class Halstead:
-
+    """
+    Classe que encapsula todas as métricas de Halstead
+    """
     def __init__(self, file):
+        """
+        Construtor de um novo objeto 'Halstead'
+        :param file: nome do arquivo
+        """
         self.file = file
         self.num_operators = 0
         self.num_operands  = 0
         self.lines_of_code = 0
 
     def check_if_file_is_valid(self):
-        return(self.file[-1] == 'c' and self.file[-2] == '.')
+        """
+        Verifica se o arquivo é .c
+        :return: bool
+        """
+        return self.file[-1] == 'c' and self.file[-2] == '.'
+
+    def count_lines_in_file(self):
+        """
+        Conta as linhas de código de um arquivo, excluindo as linhas em branco
+        :return: int
+        """
+        loc = 0
+        for i in open(self.file):
+            if i.strip():
+                loc += 1
+        return loc
 
 def main():
 
-    #Trata o argumento pela linha de comando
+    # Trata o argumento pela linha de comando
     parser = argparse.ArgumentParser()
     parser.add_argument("file", help="Nome do arquivo", type= str)
     args = parser.parse_args()
-    print(args.file)
+    print("Nome do arquivo:", args.file)
 
-    #Cria a classe que fará as verificações
+    # Cria a classe que fará as metricas
     h = Halstead(args.file)
-    print(h.check_if_file_is_valid())
+    print("Arquivo é válido?", h.check_if_file_is_valid())
+    print("LOC:", h.count_lines_in_file())
 
 
 
